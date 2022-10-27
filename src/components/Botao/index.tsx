0483f7bcd196a3599ef9b514010fd722b7543384
Pedro Messetti <pedromessetti@gmail.com>
2022-10-27 16:11:20 +0100

    start development

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Botao extends React.Component {
+  render() {
+    return (
+      <button>Botão</button>
+    )
+  }
+}
---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Lista() {
+  return (
+    <aside>
+        <h2>Estudos do dia</h2>
+        <ul>
+            <li>
+                <h3></h3>
+                <span></span>
+            </li>
+        </ul>
+    </aside>
+  )
+}
