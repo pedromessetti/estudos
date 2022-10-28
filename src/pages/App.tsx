c6f6066285920cfb3dc8c118229fa3971fcbd751
Pedro Messetti <pedromessetti@gmail.com>
2022-10-28 15:04:58 +0100

    add alterações de state entre componentes

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,29 @@
+import { useState } from 'react';
 import Cronometro from '../components/Conometro';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
+import { ITarefa } from '../types/tarefa';
 import style from './app.module.scss'
 
 export default function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([])
+  const [selecionado, setSelecionado] =useState<ITarefa>()
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada)
+    setTarefas((tarefasAnteriores) => tarefasAnteriores.map(tarefa => ({
+      ...tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })))
+  }
+
   return (
     <div className={style.AppStyle}>
-      <Formulario />
-      <Lista />
+      <Formulario setTarefas={setTarefas}/>
+      <Lista 
+        tarefas={tarefas}
+        selecionaTarefa={selecionaTarefa}
+      />
       <Cronometro />
     </div>
   );
