1c72da947abd6ba81126403f75faa56b4622d920
Pedro Messetti <pedromessetti@gmail.com>
2022-10-31 16:36:11 +0000

    correção semantica

--- src/common/utils/time.ts
+++ src/common/utils/time.ts
@@ -1,4 +1,4 @@
-export  function tempoParaSegundos(tempo: string) {
+export function tempoParaSegundos(tempo: string) {
     const [horas = '0', minutos = '0', segundos = '0'] = tempo.split(":")
 
     const horasEmSegundos = Number(horas) * 3600
